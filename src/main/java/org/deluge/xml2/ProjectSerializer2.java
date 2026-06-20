e09fc99153df699bbecc9793b0fbc282f9e244df
Ludovic Champenois <ludo@google.com>
2026-06-20 05:41:10 -0700

    Fix Kit silent bug by serializing drum pitch as bipolar parameter (centered at 0x00000000)

--- src/main/java/org/deluge/xml2/ProjectSerializer2.java
+++ src/main/java/org/deluge/xml2/ProjectSerializer2.java
@@ -144,7 +144,7 @@ public class ProjectSerializer2 {
           writeHexTagUnipolar(writer, "lpfResonance", sound.getLpfRes());
           writeHexTagFreq(writer, "hpfFrequency", sound.getHpfFreq());
           writeHexTagUnipolar(writer, "hpfResonance", sound.getHpfRes());
-          writeHexTagUnipolar(writer, "pitch", sound.getPitchSemitones());
+          writeHexTag(writer, "pitch", sound.getPitchSemitones());
           writeHexTagUnipolar(writer, "reverbAmount", sound.getReverbAmount());
           writeHexTagUnipolar(writer, "delayRate", sound.getDelayRate());
           serializeEnvelope(writer, "envelope1", sound.getAdsr());
